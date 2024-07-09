0b7d62e751e45b0ae384e91aa201b1743ef446bc
Jonathan M Paduthala <jonathanpaduthala@gmail.com>
2024-07-09 17:40:13 +0530

    built a Music PLayer

--- MusicPlayer/app/src/main/java/com/jaunty/musicplayer/MainActivity.java
+++ MusicPlayer/app/src/main/java/com/jaunty/musicplayer/MainActivity.java
@@ -1,6 +1,11 @@
 package com.jaunty.musicplayer;
 
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +13,90 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
+    MediaPlayer player;
+    AudioManager audioManager;
+    public void play (View view){
+        player.start();
+    }
+
+    public void pause (View view){
+        player.pause();
+    }
+
+    public void stop (View view){
+        player.stop();
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        player= MediaPlayer.create(this,R.raw.music);
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        int maxVol = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int curVol = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        SeekBar seekVol = findViewById(R.id.seekVol);
+        seekVol.setMax(maxVol);
+        seekVol.setProgress(curVol);
+
+        seekVol.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()    {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,progress,0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+
+
+
+        });
+
+        SeekBar seekProg = findViewById(R.id.seekProg);
+        seekProg.setMax(player.getDuration());
+
+        new Timer().scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                seekProg.setProgress(player.getCurrentPosition());
+            }
+        },0,1200);
+
+
+        seekProg.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                player.seekTo(i); //here we have used i instead of progress since the default template
+                                    // has i as the iterative variable, we could have change the attribute in function call and used progress.
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
