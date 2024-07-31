39ac658d31a4cda60d8cd8df3ddfbcdbea789a52
Jaunty Paul <jonathanpaduthala@gmail.com>
2024-07-31 12:21:39 +0530

    Toast message complete

---
+++ ToastDemo/app/src/main/java/com/jaunty/toastdemo/MainActivity.java
@@ -0,0 +1,41 @@
+package com.jaunty.toastdemo;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void login(View view)
+    {
+        EditText username = (EditText) findViewById(R.id.name);
+        Log.i("Test","Hello");
+        Toast.makeText(this, "Hello " + username.getText().toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
